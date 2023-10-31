19fc3c44856651c507c10f4adcb3254f4d2762cc
Przemek <przemek.lewtak@heywood.co.uk>
2023-10-31 23:49:34 +0000

    Updated prisma

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,13 @@ const config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: "#FF0000",
+        secondary: "#00FF00",
+      },
+      fontFamily: {
+        sans: ["Roboto", "Arial", "sans-serif"],
+      },
       container: {
         padding: {
           DEFAULT: "1rem",
